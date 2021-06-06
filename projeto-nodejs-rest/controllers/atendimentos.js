eb3eb9d708c8d13b88c64888e8e76d4f503286eb
rafaelandradegs <rafaelandradegs@gmail.com>
2021-06-06 19:30:41 -0300

    Registrando dados no BD

--- projeto-nodejs-rest/controllers/atendimentos.js
+++ projeto-nodejs-rest/controllers/atendimentos.js
@@ -1,8 +1,12 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce esta na rota de atendimento e esta realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Voce esta na rota de atendimentos e esta realizando um POST')
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post Atendimento')
     })
 }
