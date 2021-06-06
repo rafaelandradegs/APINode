48e0f60ee6c89317f925d9827cfa1a3642e59b29
rafaelandradegs <rafaelandradegs@gmail.com>
2021-06-06 15:55:33 -0300

    Configuracao do ambiente mais Express

--- projeto-nodejs-rest/index.js
+++ projeto-nodejs-rest/index.js
@@ -1,1 +1,7 @@
-import
+const express = require('express')
+
+const app = express()
+
+app.listen(3000, () => console.log ('Servidor rodando na porta 3000'))
+
+app.get('/atendimentos', (req, res) => res.send('Voce esta na rota de teste'))
