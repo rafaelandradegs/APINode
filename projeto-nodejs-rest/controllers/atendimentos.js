ea931b1791b4d870e678d3f88a7d4997c4ac63b7
rafaelandradegs <rafaelandradegs@gmail.com>
2021-06-06 23:41:31 -0300

    Tratando GET

--- projeto-nodejs-rest/controllers/atendimentos.js
+++ projeto-nodejs-rest/controllers/atendimentos.js
@@ -1,7 +1,15 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Voce esta na rota de atendimento e esta realizando um GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
